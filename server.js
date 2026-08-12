7bf8350e197fb68c7ca5849b4d22c0c52159e0fa
benoit-sigwald <benoit.p.g.sigwald@gmail.com>
2026-08-12 10:09:09 +0200

    Dashboard: Cache-Control no-cache sur la page HTML (évite le cache navigateur périmé)

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const server = http.createServer(async (req, res) => {
     res.end('ok');
     return;
   }
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8', 'Cache-Control': 'no-cache' });
   res.end(INDEX);
 });
 
